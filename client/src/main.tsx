fb714aabbeb3d31de3a2e7b2145e49ce2a7794c9
Nicola Migone <nicolamigone179@gmail.com>
2025-09-21 14:58:57 +0200

    Add routing, menu icons navigate to respective pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Index } from "./routes/index";
+import { Inventory } from "./routes/inventory";
+import { Database } from "./routes/database";
+import { Crafting } from "./routes/crafting";
+import { Map } from "./routes/map";
+import { User } from "./routes/user";
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById("root");
+
+createRoot(root!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />}></Route>
+        <Route path="/inventory" element={<Inventory />}></Route>
+        <Route path="/database" element={<Database />}></Route>
+        <Route path="/crafting" element={<Crafting />}></Route>
+        <Route path="/map" element={<Map />}></Route>
+        <Route path="/user" element={<User />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
